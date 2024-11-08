5363c5bdddae5347ad6da48d6a5bb1ce23dafd07
Nastya Zenkovetc <lebed1412@yandex.ru>
2024-11-08 08:43:06 +0300

    correct commit reveal

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -1,11 +1,10 @@
 window.addEventListener("scroll", () => {
     const reveals = document.querySelectorAll(".reveal");
-    let scroll = window.scrollY;
     
     reveals.forEach((reveal) => {
-        reveal.classList.remove("reveal_active")
+        reveal.classList.remove("reveal_active");
         const {top} = reveal.getBoundingClientRect();
-        if (top < scroll) {
+        if (top < window.innerHeight) {
             reveal.classList.add("reveal_active");
         }
     })
